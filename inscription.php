abd388e2fec56b44ad58c22fd0de18f2b3209317
Dridox <bastien.heitzmann@gmail.com>
2024-04-19 10:42:02 +0200

    ajout de la fonction pour vérifier si l'émail est déjà présent dans la bdd

--- inscription.php
+++ inscription.php
@@ -1,18 +1,41 @@
 <?php 
 
+
+function email_present($email, $bdd) {
+    $query = $bdd->prepare("SELECT email FROM utilisateur WHERE email = :email");
+    
+    $query->execute(['email' => $email]);
+    
+    $result = $query->fetch(PDO::FETCH_ASSOC);
+    
+    if ($result) {
+        return 0; // L'e-mail est déjà présent
+    } else {
+        return 1; // L'e-mail n'est pas présent
+    }
+}
 session_start();
 $id_session = session_id();
 
 
-$bdd = new PDO("mysql:host=localhost;dbname=tennis;charset=utf8", "root", "");
+try{
+    $bdd = new PDO("mysql:host=localhost;dbname=tennis;charset=utf8", "root", "");
+
+} catch (PDOException $e) {
+    die('Erreur de connexion : ' . $e->getMessage());
+}
 
 $nom = $_POST["nom"];
 $prenom = $_POST["prenom"];
 $email = $_POST["email"];
 $password = $_POST["password"];
 
-$req = $bdd->prepare("INSERT INTO utilisateur(nom, prenom, email, password) VALUES (?,?,?,?);");
-$req->execute([$nom, $prenom,$email,$password]);
+if(email_present($email,$bdd)==0){
+    echo "emial déjà présent";
+}else {
+    $req = $bdd->prepare("INSERT INTO utilisateur(nom, prenom, email, password) VALUES (?,?,?,?);");
+    $req->execute([$nom, $prenom,$email,$password]);
+}
 
 
 
