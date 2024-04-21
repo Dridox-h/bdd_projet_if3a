4653a8c87753b5736f855fd20e5590d2b167df40
Blusk <atsiz.blusk@gmail.com>
2024-04-21 22:17:56 +0200

    test de connexion et accueil

---
+++ accueil.php
@@ -0,0 +1,26 @@
+<?php
+session_start(); 
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Réservation de court de Tennis</title>
+</head>
+<body>
+    <?php if (isset($_SESSION['id_user'])) : ?>
+        <h1>Bienvenue sur le site de réservation de courts de tennis !</h1>
+    <?php else : ?>
+        <h1>Connexion</h1>
+        <form action="connexion.php" method="post">
+            <label for="email">Email : </label>
+            <input type="email" name="email" id="email" required>
+            <label for="password">Mot de passe</label>
+            <input type="password" name="password" id="password" required>
+            <input type="submit" name="submit" value="Connexion">
+            Vous n'êtes pas encore inscrit ? <a href="inscription.php">S'inscrire </a>
+        </form>
+    <?php endif; ?>
+</body>
+</html>
---
+++ connexion.php
@@ -0,0 +1,57 @@
+<?php
+session_start(); 
+
+$message = '';
+
+// Connexion à la base de données
+try {
+    $bdd = new PDO("mysql:host=localhost;dbname=tennis;charset=utf8", "root", "");
+
+
+} catch (PDOException $e) {
+    die('Erreur de connexion : ' . $e->getMessage());
+}
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['email']) && isset($_POST['password'])) {
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        
+        $req = $pdo->prepare("SELECT * FROM utilisateur WHERE email = :email");
+        $req->execute(['email' => $email]);
+        $user = $req->fetch();
+
+        // Vérifie si l'utilisateur existe et si le mot de passe correspond
+        if ($user && password_verify($password, $user['password'])) {
+            $_SESSION['id_user'] = $user['id_user'];
+            header("Location: accueil.php"); 
+            exit();
+        } else {
+            $message = 'Mauvais identifiants';
+        }
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Connexion</title>
+</head>
+<body>
+    <h1>Connexion</h1>
+    <?php if (!empty($message)): ?>
+        <p style="color:red"><?= $message ?></p>
+    <?php endif; ?>
+    <form action="connexion.php" method="post">
+        <label for="email">Email : </label>
+        <input type="email" name="email" id="email" required>
+        <label for="password">Mot de passe</label>
+        <input type="password" name="password" id="password" required>
+        <input type="submit" name="submit" value="Connexion">
+        Vous n'êtes pas encore inscrit ? <a href="inscription.php">S'inscrire </a>
+    </form>
+</body>
+</html>
