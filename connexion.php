d987325701005fd6d7c254a44745738485415df4
Dridox <bastien.heitzmann@gmail.com>
2024-04-24 14:26:50 +0200

    modif bdd

--- connexion.php
+++ connexion.php
@@ -39,6 +39,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <title>Connexion</title>
+    <link rel="stylesheet" href="./stylesheet/styles.css">
+
 </head>
 <body>
     <h1>Connexion</h1>
